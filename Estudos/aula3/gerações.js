0dc5011da2407d89ebfbe32ffb55edae4a0d8de8
Emerson Jr <166452705+Numpodser@users.noreply.github.com>
2024-04-10 19:23:41 -0300

    Add files via upload

--- Estudos/aula3/gerações.js
+++ Estudos/aula3/gerações.js
@@ -1,21 +1,21 @@
 let nasc = parent(prompt("Digite o ano de nascimento"))
 switch(true){
-    case nasc < 1945:
+    case nasc <= 1945:
         alert("Sem geração")
         break
-    case nasc >= 1945 && nasc < 1965:
+    case nasc <= 1964:
         alert("Baby Boomers")
         break
-    case nasc >=1965 && nasc < 1981:
+    case nasc <= 1980:
         alert("Geração X")
         break
-    case nasc >= 1981 && nasc < 1997:
+    case nasc <= 1996:
         alert("Geração Y ou Millennials")
         break
-    case nasc >= 1997 && nasc < 2011:
+    case nasc <= 2010:
         alert("Geração Z")
         break
-    case nasc > 2010:
+    case nasc >= 2011:
         alert("Geração Alfa")
         break
     default:
