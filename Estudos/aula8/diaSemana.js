2f81997307fd04dd1c657a95302a6a473998c885
Emerson Jr <166452705+Numpodser@users.noreply.github.com>
2024-04-17 22:02:07 -0300

    Add files via upload

---
+++ Estudos/aula8/diaSemana.js
@@ -0,0 +1,8 @@
+let dia = Number(prompt("Digite um número entre um 1 a 7 para saber o dia da semana"))
+switch(dia){
+    case 1:
+        alert("Segunda-feira")
+        break
+    case 2:
+        alert()
+}
